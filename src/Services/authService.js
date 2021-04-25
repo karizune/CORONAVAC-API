4031a35e6626d34a9d1610f76313547facddbda7
Luan Chrystian Galan <luanchrystian2@gmail.com>
2021-04-25 17:01:00 -0300

    correções no delete e em chamadas token jwt

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,39 +1,39 @@
-require('dotenv').config({path: './src/Config/.env'});
-const jwt = require('jsonwebtoken');
-const pacienteServico = require('../Services/pacienteService');
+require("dotenv").config({ path: "./src/Config/.env" });
+const jwt = require("jsonwebtoken");
+const pacienteServico = require("../Services/pacienteService");
 
 module.exports.verificarToken = async (request, response, next) => {
-    try {
-        const token = request.header('Authorization').split(' ');
-        if (token == undefined){
-            throw new Error();
-        }
-        const data = jwt.verify(token[1], process.env.JWT_KEY);
-        const paciente = await pacienteServico.buscaPacientePorEmail(data.email);
-        if (!paciente) {
-            throw new Error();
-        }
-        request.user = paciente;
-        request.token = token;
-        next();
+  try {
+    const token = request.header("Authorization").split(" ");
+    if (token == undefined) {
+      throw new Error();
     }
-    catch (error) {
-        response.status(401).send({ 'error': 'Não Autorizado' })
+    const data = jwt.verify(token[1], process.env.JWT_KEY);
+    console.log(data);
+    const paciente = await pacienteServico.buscaPacientePorEmail(data.email);
+    if (!paciente) {
+      throw new Error();
     }
-}
+    request.user = paciente;
+    request.token = token;
+    next();
+  } catch (error) {
+    response.status(401).send({ error: "Não Autorizado" });
+  }
+};
 
 module.exports.gerarToken = (email) => {
-    if(email == null){
-        return ({ auth: false, token: null, message: "Error"});
-    }
-    const token = jwt.sign({ email: email }, process.env.JWT_KEY);
-    return ({ auth: true, token: token, message : "OK!!" });
-}
+  if (email == null) {
+    return { auth: false, token: null, message: "Error" };
+  }
+  const token = jwt.sign({ email: email }, process.env.JWT_KEY);
+  return { auth: true, token: token, message: "OK!!" };
+};
 
-module.exports.verificaEmailSenha =  async (email, senha) =>{
-    const paciente =  await pacienteServico.verificaEmailSenha(email, senha);
-    if(paciente != undefined){
-        return paciente.email;
-    }
-    return null;
-}
+module.exports.verificaEmailSenha = async (email, senha) => {
+  const paciente = await pacienteServico.verificaEmailSenha(email, senha);
+  if (paciente != undefined) {
+    return paciente.email;
+  }
+  return null;
+};
